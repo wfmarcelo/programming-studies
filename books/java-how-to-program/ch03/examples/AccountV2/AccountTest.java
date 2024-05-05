b062b9646e89a14cab1b79a6c5f2e2952db40731
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-05-05 10:53:40 -0300

    ch 02

---
+++ books/java-how-to-program/ch03/examples/AccountV2/AccountTest.java
@@ -0,0 +1,14 @@
+// Fig. 3.6: AccountTest.java
+// Using the Account constructor to initialize the name instance
+// variable at the time each Account object is created.
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
+
+        System.out.printf("account1 name is: %s%n", account1.getName());
+        System.out.printf("account2 name is: %s%n", account2.getName());
+    }
+    
+}
