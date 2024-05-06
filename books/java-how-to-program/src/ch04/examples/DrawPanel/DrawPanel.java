eb0d69218cd9780afa42ee16e1974771f4ae8524
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-05-06 20:43:22 -0300

    ch04

---
+++ books/java-how-to-program/src/ch04/examples/DrawPanel/DrawPanel.java
@@ -0,0 +1,19 @@
+// Fig. 4.18: DrawPanel.java
+// Using drawLine to connect the corners of a panel
+package ch04.examples.DrawPanel;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        g.drawLine(0, 0, width, height);
+
+        g.drawLine(0, height, width, 0);
+    }
+}
---
+++ books/java-how-to-program/src/ch04/examples/ExamAnalysis/Analysis.java
@@ -0,0 +1,40 @@
+// Fig. 4.12/. Analysis.java
+// Analysis of examination results using nested control statements
+
+package ch04.examples.ExamAnalysis;
+
+import java.util.Scanner;
+/**
+ * Analysis
+ */
+public class Analysis {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+    
+        int passes = 0;
+        int failures = 0;
+        int studentCounter = 1;
+    
+        while (studentCounter <= 10)
+        {
+            System.out.print("Enter result (1 = pass, 2 = fail): ");
+            int result = input.nextInt();
+    
+            if (result == 1)
+                passes = passes + 1;
+            else
+                failures = failures + 1;
+    
+            studentCounter = studentCounter + 1;
+        }
+    
+        System.out.printf("Passed: %d%nFailed: %d%n", passes, failures);
+
+        if (passes > 8)
+            System.out.println("Bonus to instructor!");
+    }
+    
+
+    
+}
