0af7a67d8a278a2b72fe0d7540bb19e63c7fb55d
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-05-10 11:36:12 -0300

    ch07

---
+++ books/java-how-to-program/src/ch07/InitArrayV1/InitArray.java
@@ -0,0 +1,18 @@
+// Dig. 7.2: InitArray.java
+// Initializing the elements od an array to default values of zero.
+package ch07.InitArrayV1;
+
+/**
+ * InitArray
+ */
+public class InitArray {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        System.out.printf("%s%8s%n", "Index", "Value");
+
+        for (int counter = 0; counter < array.length; counter++)
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+    }
+    
+}
---
+++ books/java-how-to-program/src/ch07/InitArrayV3/InitArray.java
@@ -0,0 +1,21 @@
+// Fig. 7.4: InitArray.java
+// Calculating the values to be placed into the elements of an array.
+package ch07.InitArrayV3;
+
+/**
+ * InitArray
+ */
+public class InitArray {
+    public static void main(String[] args) {
+        final int ARRAY_LENGTH = 10;
+        int[] array = new int[ARRAY_LENGTH];
+
+        for (int counter = 0; counter < array.length; counter++)
+            array[counter] = 2 + 2 * counter;
+
+        System.out.printf("%s%8s%n", "Index", "Value");
+        for (int counter = 0; counter < array.length; counter++)
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+    }
+    
+}
