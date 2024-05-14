4696ddad8984742218395710ac7d75d55f2915ed
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-05-14 20:10:40 -0300

    exercises ch2

---
+++ books/java-how-to-program/src/ch02/exercises/exe2_20/Program.java
@@ -0,0 +1,11 @@
+package ch02.exercises.exe2_20;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+    }
+}
---
+++ books/java-how-to-program/src/ch02/exercises/exe2_22/Program.java
@@ -0,0 +1,11 @@
+package ch02.exercises.exe2_22;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.print("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.print("****");
+        System.out.println("**");
+    }
+}
