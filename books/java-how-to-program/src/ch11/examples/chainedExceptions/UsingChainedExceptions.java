e330836205f07cfcb67d886b4442bb94d3bc2524
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-05-17 20:00:35 -0300

    ch11

---
+++ books/java-how-to-program/src/ch11/examples/chainedExceptions/UsingChainedExceptions.java
@@ -0,0 +1,33 @@
+// Fig. 11.7: UsingChainedExceptions.java
+// Chained exceptions.
+package ch11.examples.chainedExceptions;
+
+public class UsingChainedExceptions {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void method1() throws Exception {
+        try {
+            method2();
+        } catch (Exception e) {
+            throw new Exception("Exception thrown in method1", e);
+        }
+    }
+
+    private static void method2() throws Exception {
+        try {
+            method3();
+        } catch (Exception e) {
+            throw new Exception("Exception tnrown in method2", e);
+        }
+    }
+
+    private static void method3() throws Exception {
+        throw new Exception("Exception thrown in method3");
+    }
+}
