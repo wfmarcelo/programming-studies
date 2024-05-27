98cb2e303baa9f9c1b9301f355673d320b15cd89
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-05-27 19:49:26 -0300

    ch13

---
+++ books/java-how-to-program/src/ch13/examples/fig13_31_32/Shapes.java
@@ -0,0 +1,20 @@
+// Fig. 13.32: Shapes.java
+// Demonstrating a general path.
+package ch13.examples.fig13_31_32;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Shapes {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Drawing 2D Shapes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ShapesJPanel shapesJPanel = new ShapesJPanel();
+        frame.add(shapesJPanel);
+        frame.setBackground(Color.WHITE);
+        frame.setSize(315, 330);
+        frame.setVisible(true);
+    }
+}
