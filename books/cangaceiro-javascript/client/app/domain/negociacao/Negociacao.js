6570a07711399b7d4d037f5e3a1029ea61f18881
Marcelo Wollmann Figueiro <marcelo.wollmann@gmail.com>
2024-06-29 14:49:39 -0300

    ch02

--- books/cangaceiro-javascript/client/app/domain/negociacao/Negociacao.js
+++ books/cangaceiro-javascript/client/app/domain/negociacao/Negociacao.js
@@ -1,7 +1,29 @@
 class Negociacao {
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor) {
+        
+        this.#data = new Date(data.getTime());
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+    get volume() {
+        return this.quantidade * this.valor;
+    }
+    
+    get data() {
+        return new Date(this.#data.getTime());
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor() {
+        return this.#valor;
     }
 }
