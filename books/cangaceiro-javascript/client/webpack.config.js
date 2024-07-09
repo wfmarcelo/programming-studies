69e8f495b77906b07f4b266d6c664198a317aedb
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-07-09 10:21:49 -0300

    gitignore

---
+++ books/cangaceiro-javascript/client/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins = [];
+
+if (process.env.NODE_ENV == 'production') {
+    plugins.push(new babiliPlugin());
+}
+
+module.exports = {
+    entry: './app-src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins
+}
