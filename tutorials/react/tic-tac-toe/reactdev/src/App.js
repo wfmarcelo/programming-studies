1789284c1dde3d82036378c5afb6bdc34c04e161
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2024-07-11 10:19:21 -0300

    extra 1 e 2

--- tutorials/react/tic-tac-toe/reactdev/src/App.js
+++ tutorials/react/tic-tac-toe/reactdev/src/App.js
@@ -11,9 +11,11 @@ function Square({ value, onSquareClick }) {
 
 
 function Board({ xIsNext, squares, onPlay }) {
-  
-  function handleClick(i){
-    if (calculateWinner(squares) || squares[i]) 
+
+  const board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i])
       return;
 
     const nextSquares = squares.slice();
@@ -25,28 +27,22 @@ function Board({ xIsNext, squares, onPlay }) {
   let status;
 
   if (winner)
-      status = `Winner: ${winner}`;
+    status = `Winner: ${winner}`;
   else
     status = `Next player: ${xIsNext ? "X" : "0"}`;
 
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {
+        board.map((rows) => (
+          <div className="board-row">
+            {
+              rows.map(column => <Square value={squares[column]} onSquareClick={() => handleClick(column)} />)
+            }
+          </div>
+        ))
+      }
     </>);
 }
 
@@ -68,10 +64,22 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    description = 
-      (move > 0) ? 
-        `Go to move # ${move}` :
-        'Go to game start';
+
+    if (move > 0) {
+      if (move === currentMove) {
+        description = `You are at move # ${move}`;
+        return (
+          <li key={move}>
+            {description}
+          </li>
+        )
+      }
+      else
+        description = `Go to move # ${move}`;
+    }
+    else {
+      description = 'Go to game start';
+    }
 
     return (
       <li key={move}>
@@ -80,7 +88,7 @@ export default function Game() {
     )
   });
 
-  return(
+  return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
@@ -107,7 +115,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
-        return squares[a];
+      return squares[a];
   }
 
   return null;
