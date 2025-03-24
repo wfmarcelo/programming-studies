5eaae3773076414c01755ff17665b34ef8ebcd9a
Marcelo Wollmann Figueiro <marcelo.wollmann@gmail.com>
2025-03-24 06:27:55 -0300

    ch02 - adequação dos dados recebidos

--- books/cangaceiro2/public/app/app.js
+++ books/cangaceiro2/public/app/app.js
@@ -1,20 +1,14 @@
-import { handleStatus } from "./utils/promise-helpers.js";
+import { handleStatus, log } from "./utils/promise-helpers.js";
+import './utils/array-helpers.js'
+import { notasService } from "./nota/service.js";
+
+
 
 document
     .querySelector('#myButton')
     .onclick = () =>
-        fetch('http://localhost:3000/notas')
-            .then(handleStatus)
-            .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-            .then(itens => {
-                console.log(itens);
-                return itens;
-            })
-            .then(itens => itens.filter(item => item.codigo == '2143'))
-            .then(itens => {
-                console.log(itens);
-                return itens;
-            })
-            .then(itens => itens.reduce((total, item) => total + item.valor, 0))
-            .then(console.log)
-            .catch(console.log);
+
+        notasService
+            .sumItems('2143')
+            .then(log)
+            .catch(log);
