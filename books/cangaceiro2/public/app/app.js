a0be41d11bf077367f257b8c4a217829aae16dc6
Marcelo Wollmann Figueiro <marcelo.wollmann@gmail.com>
2025-03-27 19:10:49 -0300

    ch 04 limitando operações

--- books/cangaceiro2/public/app/app.js
+++ books/cangaceiro2/public/app/app.js
@@ -1,14 +1,21 @@
-import { handleStatus, log } from "./utils/promise-helpers.js";
-import './utils/array-helpers.js'
 import { notasService } from "./nota/service.js";
+import './utils/array-helpers.js';
+import { debounceTime, partialize, pipe, takeUntil } from "./utils/operators.js";
+import { log } from "./utils/promise-helpers.js";
+
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
 
 
+const action = operations(() =>
+    notasService
+        .sumItems('2143')
+        .then(log)
+        .catch(log));
 
 document
     .querySelector('#myButton')
-    .onclick = () =>
+    .onclick = action;
 
-        notasService
-            .sumItems('2143')
-            .then(log)
-            .catch(log);
