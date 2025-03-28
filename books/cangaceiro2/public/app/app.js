5f9a08e4999648fe118a35cbe31d59ba5d566938
Marcelo Wollmann Figueiro <marcelo.wollmann@gmail.com>
2025-03-28 18:47:59 -0300

    ch 05 lentidão na rede

--- books/cangaceiro2/public/app/app.js
+++ books/cangaceiro2/public/app/app.js
@@ -1,17 +1,15 @@
 import { notasService } from "./nota/service.js";
 import './utils/array-helpers.js';
 import { debounceTime, partialize, pipe, takeUntil } from "./utils/operators.js";
-import { log } from "./utils/promise-helpers.js";
+import { log, retry, timeoutPromise } from "./utils/promise-helpers.js";
 
 const operations = pipe(
     partialize(takeUntil, 3),
     partialize(debounceTime, 500)
 );
 
-
 const action = operations(() =>
-    notasService
-        .sumItems('2143')
+    retry(3, 3000, () => timeoutPromise(200, notasService.sumItems('2143')))
         .then(log)
         .catch(log));
 
--- books/cangaceiro2/public/app/utils/promise-helpers.js
+++ books/cangaceiro2/public/app/utils/promise-helpers.js
@@ -5,3 +5,27 @@ export const log = param => {
     console.log(param);
     return param;
 }
+
+export const timeoutPromise = (milliseconds, promise) => {
+    const timeout = new Promise((resolve, reject) =>
+        setTimeout(() =>
+            reject(`Limire da promise excedido (limite: ${milliseconds} ms)`), milliseconds));
+
+    return Promise.race([
+        timeout,
+        promise
+    ]);
+};
+
+export const delay = milliseconds => data =>
+    new Promise(resolve =>
+        setTimeout(() => resolve(data), milliseconds)
+    );
+
+export const retry = (retries, milliseconds, fn) =>
+    fn().catch(err => {
+        console.log(retries);
+        return delay(milliseconds)().then(() => retries > 1
+            ? retry(--retries, milliseconds, fn)
+            : Promise.reject(err))
+    });
