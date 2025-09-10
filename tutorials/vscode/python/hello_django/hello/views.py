2ce620849594c586be9029cbaf4c98391df5a11f
Marcelo Wollmann Figueiró <marcelo.wollmann@uniarp.edu.br>
2025-09-10 13:44:31 -0300

    working with templates

---
+++ tutorials/vscode/python/hello_django/hello/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, "hello/home.html")
+
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri())
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+
